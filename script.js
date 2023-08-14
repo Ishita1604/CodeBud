393c2139a50f1b8c090896f6767a5016aba5edc3
Ishita <ishitagupta1604@gmail.com>
2023-08-14 12:56:32 +0530

    Updated version

--- script.js
+++ script.js
@@ -7,21 +7,16 @@ fetch("https://kontests.net/api/v1/all").then(
 
                     var temp = "";
                     data.forEach((itemData) => {
+                        const startTime = new Date(itemData.start_time).toLocaleString();
+                        const endTime = new Date(itemData.end_time).toLocaleString();
 
                         temp += '<tr class="row">';
-                        //display only item data
-                        // temp += "<td>" + itemData.name + "</td>";
-                        //display both name & url in one 
                         temp += "<td>" + `<a href="${itemData.url}" target="_blank">` + itemData.name + `</a>` + "</td>";
-                        //display only item url
-                        //temp += `<td>` + `<a href="${itemData.url}" target="_blank" rel="noopenner">` + itemData.url + `</a></td>`;
-                        temp += '<td >' + `<button class="start">` + itemData.start_time + "</button></td>";
-                        temp += '<td >' + `<button class="end">` + itemData.end_time + "</button></td>";
+                        temp += '<td >' + `<button class="start">` + startTime + "</button></td>";
+                        temp += '<td >' + `<button class="end">` + endTime + "</button></td>";
                         temp += "<td>" + itemData.duration + "</td>";
                         temp += "<td>" + itemData.site + "</td>";
                         temp += "<td>" + itemData.status + "</td></tr>";
-
-
                     });
 
                     document.getElementById('data').innerHTML = temp;
